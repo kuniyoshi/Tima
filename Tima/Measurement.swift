d0c7d071548325c4896c672488ba8c9f1dffefea
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-12-29 14:12:22 +0900

    change date format to ISO-8601

--- Tima/Measurement.swift
+++ Tima/Measurement.swift
@@ -3,6 +3,14 @@ import SwiftData
 
 @Model
 final class Measurement: Codable {
+    private static let iso8601DateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = .autoupdatingCurrent
+        return formatter
+    }()
+
     var id: UUID
     var genre: String
     var work: String
@@ -13,14 +21,6 @@ final class Measurement: Codable {
         end.timeIntervalSince(start)
     }
 
-    init(id: UUID = UUID(), genre: String, work: String, start: Date, end: Date) {
-        self.id = id
-        self.genre = genre
-        self.work = work
-        self.start = start
-        self.end = end
-    }
-
     enum CodingKeys: String, CodingKey {
         case id
         case genre
@@ -29,13 +29,30 @@ final class Measurement: Codable {
         case end
     }
 
+    init(id: UUID = UUID(), genre: String, work: String, start: Date, end: Date) {
+        self.id = id
+        self.genre = genre
+        self.work = work
+        self.start = start
+        self.end = end
+    }
+
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(UUID.self, forKey: .id)
         genre = try container.decode(String.self, forKey: .genre)
         work = try container.decode(String.self, forKey: .work)
-        start = try container.decode(Date.self, forKey: .start)
-        end = try container.decode(Date.self, forKey: .end)
+
+        let startString = try container.decode(String.self, forKey: .start)
+        let endString = try container.decode(String.self, forKey: .end)
+
+        guard let startDate = Measurement.iso8601DateFormatter.date(from: startString),
+              let endDate = Measurement.iso8601DateFormatter.date(from: endString) else {
+            throw DecodingError.dataCorruptedError(forKey: .start, in: container, debugDescription: "Could not decode start: \(startString)")
+        }
+
+        start = startDate
+        end = endDate
     }
 
     func encode(to encoder: Encoder) throws {
@@ -43,7 +60,7 @@ final class Measurement: Codable {
         try container.encode(id, forKey: .id)
         try container.encode(genre, forKey: .genre)
         try container.encode(work, forKey: .work)
-        try container.encode(start, forKey: .start)
-        try container.encode(end, forKey: .end)
+        try container.encode(Measurement.iso8601DateFormatter.string(from: start), forKey: .start)
+        try container.encode(Measurement.iso8601DateFormatter.string(from: end), forKey: .end)
     }
 }
