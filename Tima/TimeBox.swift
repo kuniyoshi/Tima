26c69e2240d1a2fdf9b03fdf45eca5bd3baa8cc9
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-01-01 20:38:17 +0900

    change time format

--- Tima/TimeBox.swift
+++ Tima/TimeBox.swift
@@ -18,13 +18,20 @@ final class TimeBox: Codable {
 
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        start = try container.decode(Date.self, forKey: .start)
+
+        let startString = try container.decode(String.self, forKey: .start)
+
+        guard let startDate = Util.iso8601DateFormatter.date(from: startString) else {
+            throw DecodingError.dataCorruptedError(forKey: .start, in: container, debugDescription: "Could not decode start: \(startString)")
+        }
+
+        start = startDate
         workMinutes = try container.decode(Int.self, forKey: .workMinutes)
     }
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(start, forKey: .start)
+        try container.encode(Util.iso8601DateFormatter.string(from: start), forKey: .start)
         try container.encode(workMinutes, forKey: .workMinutes)
     }
 }
