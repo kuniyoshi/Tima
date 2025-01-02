2a427bd536f3c305489cd40775239e7a57051ddc
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-01-02 20:50:22 +0900

    tidy up code

--- Tima/ContentView.swift
+++ Tima/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
             }
             .pickerStyle(.segmented)
             .padding()
-            .keyboardShortcut(.space, modifiers: .command)
 
             Button(action: {
                 selectedView = 0
