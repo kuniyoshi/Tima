0a152130f7f87390d0a4bac521bdd6ff80a7bf4f
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-01-05 15:46:20 +0900

    use NavigationStack

--- Tima/ContentView.swift
+++ Tima/ContentView.swift
@@ -4,41 +4,27 @@ struct ContentView: View {
     @State private var selectedView = 0
 
     var body: some View {
-        VStack {
-            Picker(selection: $selectedView, label: EmptyView()) {
-                Text("Measurement").tag(0)
-                Text("TimeBox").tag(1)
+        NavigationStack {
+            VStack {
+                if selectedView == 0 {
+                    MeasurementView()
+                } else {
+                    TimeBoxView()
+                }
             }
-            .pickerStyle(.segmented)
-            .padding()
+            .toolbar {
+                ToolbarItemGroup(placement: .automatic) {
+                    Button("Measurement") {
+                        selectedView = 0
+                    }
+                    .keyboardShortcut("1", modifiers: .command)
 
-            Button(action: {
-                selectedView = 0
-            }) {
-                Text("Select measurement")
+                    Button("TimeBox") {
+                        selectedView = 1
+                    }
+                    .keyboardShortcut("2", modifiers: .command)
+                }
             }
-            .keyboardShortcut("1", modifiers: .command)
-            .frame(width: 0, height: 0)
-            .opacity(0)
-
-            Button(action: {
-                selectedView = 1
-            }) {
-                Text("Select timeBox")
-            }
-            .keyboardShortcut("2", modifiers: .command)
-            .frame(width: 0, height: 0)
-            .opacity(0)
-
-            Spacer()
-
-            if selectedView == 0 {
-                MeasurementView()
-            } else {
-                TimeBoxView()
-            }
-
-            Spacer()
         }
     }
 }
