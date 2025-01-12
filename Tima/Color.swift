f13c4273c05a56c407ce826762c47bb0f7ba0dbc
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-01-12 20:42:02 +0900

    add more colors

--- Tima/Color.swift
+++ Tima/Color.swift
@@ -2,15 +2,22 @@ import SwiftUI
 
 // Codable Color to export application's data
 struct Color: Codable, Hashable, Identifiable {
-    static let red = Self(red: 1.0, green: 0.0, blue: 0.0)
-    static let magenta = Self(red: 1.0, green: 0.0, blue: 1.0)
-    static let cyan = Self(red: 0.0, green: 1.0, blue: 1.0)
-    static let green = Self(red: 0.0, green: 1.0, blue: 0.0)
     static let blue = Self(red: 0.0, green: 0.0, blue: 1.0)
+    static let brown = Self()
+    static let cyan = Self(red: 0.0, green: 1.0, blue: 1.0)
     static let gray = Self(red: 0.5, green: 0.5, blue: 0.5)
+    static let green = Self(red: 0.0, green: 1.0, blue: 0.0)
+    static let indigo = Self()
+    static let mint = Self()
+    static let orange = Self()
+    static let pink = Self()
+    static let purple = Self()
+    static let red = Self(red: 1.0, green: 0.0, blue: 0.0)
+    static let teal = Self(red: 1.0, green: 0.0, blue: 0.0)
+    static let yellow = Self(red: 1.0, green: 0.0, blue: 0.0)
 
     static var allCases: [Self] {
-        [red, magenta, cyan, green, blue, gray]
+        [blue, brown, cyan, gray, green, indigo, mint, orange, pink, purple, red, teal, yellow]
     }
 
     var red: Double
