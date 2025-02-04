60dee8708f509eac972d3cd81bb268f566838876
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-02-04 20:24:54 +0900

    wip

--- Tima/Database.swift
+++ Tima/Database.swift
@@ -7,11 +7,29 @@ final class Database: ObservableObject {
     static let shared = Database()
 
     @Published private(set) var measurements: [Measurement] = []
-    @Published private(set) var groupedMeasurements: [[Measurement]] = []
+    @Published private(set) var groupedMeasurements: [[(Measurement, Tima.Task)]] = []
     @Published private(set) var tasks: [Tima.Task] = []
 
     private var modelContext: ModelContext
     private var cancellables = Set<AnyCancellable>()
+
+    func foo(measurements: [Measurement]) -> [[(Measurement, Tima.Task)]] {
+        let dictionary = Dictionary(grouping: measurements, by: { measurement in
+            Calendar.current.startOfDay(for: measurement.start)
+        })
+        let pairs = dictionary.sorted { $0.key > $1.key }
+        let groups = pairs.map(\.1)
+        return groups.map { values in
+            values
+                .sorted { $0.start > $1.start }
+                .compactMap { item in
+                    if let task = tasks.first(where: { $0.name == item.taskName }) {
+                        return (item, task)
+                    }
+                    return nil
+                }
+        }
+    }
 
     private init() {
         do {
@@ -25,16 +43,36 @@ final class Database: ObservableObject {
             modelContext = container.mainContext
 
             $measurements.map { measurements in
-                let dictionary = Dictionary(grouping: measurements, by: { measurement in
-                    Calendar.current.startOfDay(for: measurement.start)
-                })
-
-                return dictionary
-                    .sorted { $0.key > $1.key }
-                    .map { $0.value.sorted { $0.start > $1.start } }
+                foo(measurements: measurements)
+//                let dictionary = Dictionary(grouping: measurements, by: { measurement in
+//                    Calendar.current.startOfDay(for: measurement.start)
+//                })
+//                let pairs = dictionary
+//                    .sorted { $0.key > $1.key }
+//                let grouped = pairs
+//                    .map { (_, groups) in
+//                        groups.map { values in
+//                            values
+//                                .sorted { $0.start > $1.start }
+//                                .compactMap { item in
+//                                    if let task = tasks.first(where: { $0.name == item.taskName }) {
+//                                        return (values.sorted { $0.start > $1.start }, task)
+//                                    }
+//                                    return nil
+//                                }
+//                        }
+//                    }
+//                return grouped
+//                    .compactMap { items in
+//                        if items.count > 0 {
+//                            return items
+//                        } else {
+//                            return nil
+//                        }
+//                    }
             }
             .sink { [unowned self] newValue in
-                self.groupedMeasurements = newValue
+//                self.groupedMeasurements = newValue
             }
             .store(in: &cancellables)
         } catch {
