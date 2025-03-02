f21a207b4144cf6d428fa9358038e3ae43396941
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-03-02 14:46:30 +0900

    add daily notification settings keys

--- Tima/SettingsKeys.swift
+++ Tima/SettingsKeys.swift
@@ -1,7 +1,13 @@
 // Application settings keys
 enum SettingsKeys {
+    enum Measurement: String {
+        case dailyWorkMinutes = "measurement.dailyWorkMinutes"
+        case dailyEndTitle = "measurement.dailyEndTitle"
+        case dailyEndMessage = "measurement.dailyEndMessage"
+    }
+
     enum TimeBox: String {
-        case isSoundNotification = "timeBox.isSoundNotification"
+        case isSoundNotification = "timeBox.isSoundNotification" // TODO: commonize
         case isBannerNotification = "timeBox.isBannerNotification"
         case workMinutes = "timeBox.workMinutes"
         case breakMinutes = "timeBox.breakMinutes"
