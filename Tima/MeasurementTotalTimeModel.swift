f92615da257c5956cf5d6cc4619871f85f30d4ea
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-03-02 14:37:39 +0900

    add a notification on finish daily work

--- Tima/MeasurementTotalTimeModel.swift
+++ Tima/MeasurementTotalTimeModel.swift
@@ -1,10 +1,39 @@
+import Combine
+import UserNotifications
 import SwiftUI
 
 class MeasurementTotalTimeModel: ObservableObject {
     @Published private(set) var totalMinutes: Int
+    let notificationPublisher = PassthroughSubject<UNMutableNotificationContent, Never>()
+    private var lastPublishedAt: Date?
+    private var cancellables: Set<AnyCancellable> = []
+
+    private var notification: UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = "Yeah!" // TODO: settings
+        content.body = "Today's work is over!"
+        content.sound = nil
+        return content
+    }
 
     init(totalMinutes: Int = 0) {
         self.totalMinutes = totalMinutes
+
+        $totalMinutes
+            .sink { [unowned self] newValue in
+                if newValue < 4 { // TODO: settings
+                    return
+                }
+
+                let calendar = Calendar.current
+                if let lastPublishedAt = self.lastPublishedAt,
+                   calendar.isDate(lastPublishedAt, inSameDayAs: Date()) {
+                    return
+                }
+                notificationPublisher.send(notification)
+                lastPublishedAt = Date()
+            }
+            .store(in: &cancellables)
     }
 
     func setValue(_ value: Int) {
--- Tima/MeasurementTotalTimeView.swift
+++ Tima/MeasurementTotalTimeView.swift
@@ -1,7 +1,10 @@
+import Combine
 import SwiftUI
+import UserNotifications
 
 struct MeasurementTotalTimeView: View {
     @StateObject private var model: MeasurementTotalTimeModel
+    @State private var cancellables = Set<AnyCancellable>()
 
     var body: some View {
         HStack {
@@ -15,11 +18,37 @@ struct MeasurementTotalTimeView: View {
                 .font(.caption)
         }
         .padding(.horizontal)
+        .onAppear {
+            model.notificationPublisher.sink { notification in
+                notify(content: notification)
+            }
+            .store(in: &cancellables)
+        }
     }
 
     init(model: MeasurementTotalTimeModel) {
         _model = .init(wrappedValue: model)
     }
+
+    private func notify(content: UNMutableNotificationContent) {
+        // TODO: settings where notify or not
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(
+            identifier: "notification", // TODO: commonize
+            content: content,
+            trigger: trigger
+        )
+
+        Task {
+            do {
+                try await UNUserNotificationCenter.current().add(request)
+            } catch {
+                print("Could not add notification: \(error.localizedDescription)")
+            }
+        }
+    }
+
 }
 
 #Preview {
