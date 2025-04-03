227ed3e8059ccb56e7ece3736ebbb052fcdff439
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-04-03 20:57:08 +0900

    add a script

---
+++ bin/generate_sample.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :utf8 :std );
+use Data::Dumper;
+
+
+
+exit;
+
+__END__
+{
+    timeBoxes: [
+        {
+            workMinutes: 3,
+            start: "2025-03-07T20:45:35.812+09:00",
+        },
+    ],
+    imageColors: [
+        {
+            color: [
+                0.6,
+                1,
+                0.8
+            ],
+            name: "asdf",
+        },
+    ],
+    measurements: [
+        {
+            "start": "2025-03-02T10:31:47.392+09:00",
+            "id": "D9E2F98E-44F3-4CB6-8361-8E708BCF5837",
+            "work": "asdf",
+            "detail": "fdsa",
+            "end": "2025-03-02T10:34:13.884+09:00"
+        },
+    ],
+}
