b5537a7fd379fb38ace0dcaaeb794ee842895c73
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-04-05 15:10:16 +0900

    sample generator

--- bin/generate_sample.pl
+++ bin/generate_sample.pl
@@ -6,21 +6,77 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 use Time::Piece qw( );
-use Time::Seconds qw( ONE_DAY ONE_HOUR );
+use Time::Seconds qw( ONE_DAY ONE_HOUR ONE_MINUTE );
 use JSON;
+use Data::UUID;
+use Readonly;
 
-my @timeBoxes = map { timeBoxes( $_ ) }
-                map { scalar Time::Piece->localtime( time - $_ * ONE_DAY ) }
-                reverse
-                1 .. 40;
-die Dumper \@timeBoxes;
+Readonly my $TIME_BOX_DAYS => 40;
+Readonly my $MEASUREMENT_DAYS => 40;
+Readonly my $IMAGE_COLOR_SIZE => 40;
+Readonly my $RANDOM_COUNT_TIME_BOXES_PER_DAY => 7;
+Readonly my $RANDOM_COUNT_MEASUREMENTS_PER_DAY => 10;
+
+my $ug = Data::UUID->new;
+
+my @time_boxes = map { time_boxes( $_, 5 + int( 7 * rand ) ) }
+                 map { scalar Time::Piece->localtime( time - $_ * ONE_DAY ) }
+                 reverse
+                 1 .. $TIME_BOX_DAYS;
+my @image_colors = map { { name => $_, color => random_color( ) } }
+                   unique_works( map { random_work( ) } 1 .. $IMAGE_COLOR_SIZE );
+my @measurements = map { measurements( $_, \@image_colors, int( $RANDOM_COUNT_MEASUREMENTS_PER_DAY * rand ) ) }
+                   map { scalar Time::Piece->localtime( time - $_ * ONE_DAY ) }
+                   reverse
+                   1 .. $MEASUREMENT_DAYS;
+
+say JSON->new->encode( {
+    timeBoxes => \@time_boxes,
+    imageColors => \@image_colors,
+    measurements => \@measurements,
+} );
 
 
 exit;
 
-sub timeBoxes {
+sub measurements {
+    my $date = shift;
+    my @image_colors = @{ shift( ) };
+    my $count = shift;
+    my @characters = "a" .. "z";
+
+    return map {
+        my $image_color = $image_colors[ @image_colors * rand ];
+        my $start = ( $date - ( $date->epoch % ONE_DAY ) ) + ( 8 + $_ * ONE_HOUR );
+        my $end = $start + 45 * ONE_MINUTE;
+        {
+            id => $ug->to_string( $ug->create ),
+            work => $image_color->{name},
+            detail => join( q{}, map { $characters[ @characters * rand ] } 1 .. ( 1 + 8 * rand ) ),
+            start => $start->datetime . ".000+09:00",
+            end => $end->datetime . ".000+09:00",
+        };
+    } 1 .. $count;
+}
+
+sub random_color {
+    return [ map { int( rand( ) * 10 + 0.5 ) / 10 } 1 .. 3 ];
+}
+
+sub unique_works {
+    my @works = @_;
+    my %count;
+    return grep { !$count{ $_ }++ } @works;
+}
+
+sub random_work {
+    my @characters = "a" .. "z";
+    return join q{}, map { $characters[ $_ ] } map { int( rand( ) * @characters ) } 1 .. ( 1 + rand( ) * 6 );
+}
+
+sub time_boxes {
     my $time = shift;
-    my $count = 5 + int( 7 * rand );
+    my $count = shift;
 
     return map {
         my $now = $time - ( $time->epoch % ONE_DAY ) + $_ * ONE_HOUR;
